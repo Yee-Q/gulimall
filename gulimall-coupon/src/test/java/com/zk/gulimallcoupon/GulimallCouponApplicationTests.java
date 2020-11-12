844b2a5b9a97da8f1f4fab1a6c1d222c6f35a12e
Yee-Q <1051195623@qq.com>
2020-11-13 00:26:48 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/zk/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zk.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
