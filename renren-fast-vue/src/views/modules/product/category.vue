1751ee1b3b95ddd812453fa6d0009c4cc1110b4f
Yee-Q <1051195623@qq.com>
2020-11-21 15:02:07 +0800

    p53

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -7,18 +7,25 @@
         <span>
           <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">Append</el-button>
           <el-button v-if="node.childNodes.length === 0" type="text" size="mini" @click="() => remove(node, data)">Delete</el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">Edit</el-button>
         </span>
       </span>
     </el-tree>
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+    <el-dialog :title="title" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory()">确 定</el-button>
+        <el-button type="primary" @click="submitData()">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -29,7 +36,9 @@
   export default {
     data() {
       return {
-        category: {name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0},
+        title: "",
+        dialogType: "", // edit, add
+        category: {name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0, productUnit: "", icon: "", catId: null},
         dialogVisible: false,
         menus: [],
         expandedKey: [2],
@@ -40,11 +49,47 @@
       };
     },
     methods: {
+      submitData() {
+        if (this.dialogType === "add") {
+          this.addCategory()
+        }
+        if (this.dialogType === "edit") {
+          this.editCategory();
+        }
+      },
+      // 修改三级分类数据
+      editCategory() {
+        let {catId, name, icon, productUnit} = this.category;
+        let data = {catId: catId, name: name, icon: icon, productUnit: productUnit};
+        this.$http({
+          url: this.$http.adornUrl("/product/category/update"),
+          method: "post",
+          data: this.$http.adornData(data, false)
+        }).then(({data}) => {
+          this.$message({
+            message: "菜单修改成功",
+            type: "success"
+          });
+          // 关闭对话框
+          this.dialogVisible = false;
+          // 刷新出新的菜单
+          this.getMenus();
+          // 设置需要默认展开的菜单
+          this.expandedKey = [this.category.parentCid];
+        });
+      },
       append(data) {
         console.log("append", data);
+        this.dialogType = "add";
+        this.title = "添加分类";
         this.dialogVisible = true;
         this.category.parentCid = data.catId;
         this.category.catLevel = data.catLevel + 1;
+
+        this.category.name = "";
+        this.category.catId = null;
+        this.category.icon = "";
+        this.category.productUnit = "";
       },
       // 添加三级分类
       addCategory() {
@@ -67,7 +112,7 @@
         });
       },
       remove(node, data) {
-        var ids = [data.catId];
+        let ids = [data.catId];
         this.$confirm(`是否删除【${data.name}】菜单?`, '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
@@ -89,6 +134,25 @@
           });
         }).catch(() => {
 
+        });
+      },
+      edit(data) {
+        console.log("要修改的数据");
+        this.dialogType = "edit";
+        this.title = "修改分类";
+        this.dialogVisible = true;
+        // 发送请求获取当前节点的最新数据
+        this.$http({
+          url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+          method: "get",
+        }).then(({data}) => {
+          // 请求成功
+          console.log("要回显的数据", data);
+          this.category.name = data.data.name;
+          this.category.catId = data.data.catId;
+          this.category.icon = data.data.icon;
+          this.category.productUnit = data.data.productUnit;
+          this.category.parentCid = data.data.parentCid;
         });
       },
       getMenus() {
